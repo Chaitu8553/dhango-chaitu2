38534bd69641a5467bdb75865f9a67ff7d8cf902
chaitu8553 <chaitanyayelijerla@gmail.com>
2024-11-02 17:08:19 +0530

    firts commit

---
+++ SecondProject/FirstApp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render;
+
+
+
+
+
+from django.shortcuts import render
+
+# Create your views here.
+def wishes(request):
+    return render(request, 'FirstApp/wishes.html')
+def hello(request):
+	return render(request,'FirstApp/hello.html')
+import datetime;
+import time;
+def datetimefunction(request):
+	date1 = datetime.datetime.now();
+	print(date1)
+	date2 = time.ctime();
+	dict1={"server_datetime":date1,"server_datetime2":date2}
+	return render(request,'FirstApp/datetime.html',context=dict1)
+
+
+import datetime
+import time
+def student_datetime(request):
+	date1=datetime.datetime.now();
+	date2=time.ctime();
+	rollno = 1001;
+	sname='sai';
+	marks=95;
+	dict1 = {'server_datetime':date1,'server_datetime2':date2,'rollno':rollno,'sname':sname,'marks':marks};
+	return render(request,'FirstApp/studentdatetime.html',dict1);
+
+
+
